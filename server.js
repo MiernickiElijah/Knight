653c2021a5210764a49a162a8685a761e800e6a6
Elijah Miernicki <miernickielijah@gmail.com>
2021-06-29 10:53:17 -0500

    added fs read file and pathway to get

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static('public'));
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// require('./routes/apiRoutes')(app);
+require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
 // LISTENER
